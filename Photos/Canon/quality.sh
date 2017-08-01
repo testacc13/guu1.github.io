0de2097d46a6fd0adcf5c6a4a2386efd3b4d0eb0
Chickenmcheese <lulz.ga87@gmail.com>
2017-08-01 20:42:30 +0100

    Content

--- Photos/Canon/quality.sh
+++ Photos/Canon/quality.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 FILES=~/Desktop/MySiteBlog/Photos/Canon/*
 name=''
+echo 'Coverting your images...'
 for f in $FILES
-
 do	
+	echo 'Converting' $name
 	name=`basename -s p $f`	
-	#echo $name
 	convert $name -quality 50 $name
+	echo 'Done'
 done
